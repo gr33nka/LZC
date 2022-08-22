53eb0c001106ba6fa17a9ddf969695678083f598
gr33nka <gr33nka@icloud.com>
2022-08-22 03:18:46 +0300

    tiny

--- sim/run.sh
+++ sim/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 rm -f a.lst a.out dump.vcd
-sed -i 's/TIME_SIM #b[0-9]b/TIME_SIM #$1/g' ../bench/tb_top.sv
+#sed -i 's/TIME_SIM #b[0-9]b/TIME_SIM #$1/g' ../bench/tb_top.sv
 iverilog -g2012 -s tb_top ../bench/tb_top.sv
 vvp -l a.lst -n a.out -vcd
